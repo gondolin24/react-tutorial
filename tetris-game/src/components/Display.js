58c1b9ed7925ded3fbae39e9d095543739935109
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2019-10-26 22:43:41 -0400

    component init

---
+++ tetris-game/src/components/Display.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Diaplay =({gameOver, text})=>(
+<div>{text}</div>
+)
+export default Diaplay;
---
+++ tetris-game/src/components/StartButton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const StartButton = ({callBack}) => (
+    <div>
+        Start Game
+    </div>
+);
+export default StartButton;
